fe338bd900e202c97c0606148442945ffdb49b8e
Gabe Hollifield <gholl467@gmail.com>
2025-10-11 14:25:07 -0700

    Updated form background color

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -3,8 +3,8 @@ import { SearchProps } from "../types/types"
 export default function Search({ placeholder }: SearchProps) {
     return (
         <form method="GET" className="flex gap-x-4 justify-center">
-            <input placeholder={placeholder} className="p-2 bg-blue-200" name="symbol" />
-            <button className="py-1 px-2 bg-blue-200" type="submit">Submit</button>
+            <input placeholder={placeholder} className="p-2 bg-blue-100" name="symbol" />
+            <button className="py-1 px-2 bg-blue-100" type="submit">Submit</button>
         </form>
     )
 }
