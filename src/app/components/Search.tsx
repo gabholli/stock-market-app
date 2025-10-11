3595af3fa9c9c501ab42b9d1be9cd5ea2e93e6fe
Gabe Hollifield <gholl467@gmail.com>
2025-10-11 15:42:09 -0700

    Added border and border radius to form elements

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -3,8 +3,8 @@ import { SearchProps } from "../types/types"
 export default function Search({ placeholder }: SearchProps) {
     return (
         <form method="GET" className="flex gap-x-4 justify-center">
-            <input placeholder={placeholder} className="p-2 bg-blue-100" name="symbol" />
-            <button className="py-1 px-2 bg-blue-100" type="submit">Submit</button>
+            <input placeholder={placeholder} className="p-2 bg-blue-100 border-2 rounded-xl" name="symbol" />
+            <button className="py-1 px-2 bg-blue-100 border-2 rounded-xl" type="submit">Submit</button>
         </form>
     )
 }
