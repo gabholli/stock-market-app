d9920b5729fb451439261550312ae1ae25affe5b
Gabe Hollifield <gholl467@gmail.com>
2025-10-10 19:51:17 -0700

    Wrapped project in a wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,10 @@
-"use client"
+import { Suspense } from "react"
+import PortfolioClient from "./components/PortfolioClient"
 
-import axios from "axios"
-import { useSearchParams } from "next/navigation"
-import { useEffect, useState } from "react"
-import { PortfolioItem } from "./types/types"
-import Search from "./components/Search"
-
-export default function Home() {
-    const searchParams = useSearchParams()
-    const [portfolio, setPortfolio] = useState<PortfolioItem[]>([])
-    const [list, setList] = useState([])
-
-    useEffect(() => {
-        const symbolQuery = searchParams.get("symbol")
-        if (!symbolQuery) return
-
-        axios.get("/api/stockData?" + new URLSearchParams({ symbol: symbolQuery }).toString())
-            .then((response) => {
-                console.log('API Response:', response.data)
-                setPortfolio([response.data])
-            }).catch((error: string) => {
-                console.error('API Error:', error)
-            })
-    }, [searchParams])
-
-    const portfolioData = portfolio?.map((item) => {
-        return (
-            <div key={item.symbol}
-                className="border-2 text-center p-2">
-                <div className="border-b-2 p-1">
-                    <h1>{item.symbol}</h1>
-                    <p>{item.name}</p>
-                </div>
-                <div className="p-1">
-                    <p>{item.change}</p>
-                    <p>{item.percent_change}%</p>
-                </div>
-            </div>
-        )
-    })
-
+export default function Page() {
     return (
-        <div>
-            <Search placeholder="Enter symbol..." />
-            <main className="mt-6">
-                {portfolioData}
-            </main>
-        </div>
+        <Suspense fallback={<div>Loading portfolio...</div>}>
+            <PortfolioClient />
+        </Suspense>
     )
 }
