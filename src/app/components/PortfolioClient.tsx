826221fca28be9e5309e58d083841f44e6aafd38
Gabe Hollifield <gholl467@gmail.com>
2025-10-17 15:45:35 -0700

    Update PortfolioClient.tsx

--- src/app/components/PortfolioClient.tsx
+++ src/app/components/PortfolioClient.tsx
@@ -24,6 +24,7 @@ const PortfolioClient = () => {
     // }, [searchParams])
 
     const handleSubmit = (e: React.FormEvent) => {
+        console.log("Clicked")
         e.preventDefault()
         const symbolQuery = searchParams.get("symbol")
         if (!symbolQuery) return
