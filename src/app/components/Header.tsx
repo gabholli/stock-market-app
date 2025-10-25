667d4e2569cee8ef4d343a5fba9b12f617c8487e
Gabe Hollifield <gholl467@gmail.com>
2025-10-25 12:17:08 -0700

    Changed header text

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
         <header className="mb-8 flex flex-col justify-center items-center gap-y-8">
 
             <div className="flex justify-center items-center">
-                <h1 className="text-3xl md:text-3xl lg:text-4xl font-bold text-center">Financial Data App</h1>
+                <h1 className="text-3xl md:text-3xl lg:text-4xl font-bold text-center">Stock Market App</h1>
             </div>
 
         </header>
